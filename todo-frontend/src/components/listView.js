9baf68624137a40728441da329656c47bf903d90
zainm <zain@datainsights.ae>
2018-10-27 17:26:01 +0500

    add dynamic listing.

--- todo-frontend/src/components/listView.js
+++ todo-frontend/src/components/listView.js
@@ -1,49 +1,56 @@
 import React, { Component } from "react";
-import { Table } from "react-bootstrap";
-
+import { Table, Button } from "react-bootstrap";
 class ListView extends Component {
   render() {
+      const data = [
+          {
+              title : "one",
+              content : "two",
+              date : "10/27/2018"
+          },
+          {
+              title : "one1",
+              content : "two",
+              date : "10/27/2018"
+          },
+          {
+              title : "one11",
+              content : "two",
+              date : "10/27/2018"
+          },
+          {
+              title : "one111",
+              content : "two",
+              date : "10/27/2018"
+          }
+      ]
     return (
       <Table responsive>
         <thead>
           <tr>
             <th>#</th>
-            <th>Table heading</th>
-            <th>Table heading</th>
-            <th>Table heading</th>
-            <th>Table heading</th>
-            <th>Table heading</th>
-            <th>Table heading</th>
+            <th>Title</th>
+            <th>Content</th>
+            <th>Date</th>
+            <th>Edit</th>
+            <th>Delete</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-          </tr>
+          {data.map((d, i) => {
+            return <tr key={i.toString()}>
+              <td>{i+1}</td>
+              <td>{d.title}</td>
+              <td>{d.content}</td>
+              <td>{d.date}</td>
+              <td>
+                <Button bsStyle="primary">Edit</Button>
+              </td>
+              <td>
+                <Button bsStyle="danger">Delete</Button>
+              </td>
+            </tr>;
+          })}
         </tbody>
       </Table>
     );
