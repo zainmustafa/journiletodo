970fba861898a052662bc3a22554c8ad3dd1ff65
zainm <zain@datainsights.ae>
2018-10-27 18:39:03 +0500

    add api's for todo.

--- todo-backend/routes/index.js
+++ todo-backend/routes/index.js
@@ -1,9 +1,60 @@
 var express = require('express');
 var router = express.Router();
+var Task = require("./models/todos");
+
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/todos/:id', function(req, res,){
+  Task.findById(req.params.id, (err, doc) => {
+    if (err) {
+        res.send(err);
+    }
+    res.json(doc);
+  });
+})
+
+router.get('/todos', function(req, res, next) {
+    Task.find({}, (err, docs) => {
+      if(err){
+          res.send(err);
+      }
+      res.json(docs);
+  });
+});
+
+router.post('/todos', function(req, res,){
+  let newTask = new Task(req.body);
+  newTask.save((err, doc) => {
+      if (err) {
+          res.send(err);
+      }
+      res.send(doc);
+  });
+
+})
+
+router.delete('/todos/:id', function(req, res,){
+  Task.deleteOne({ _id: req.params.id }, (err, doc) => {
+    if (err) {
+        res.send(err);
+    }
+    res.json({ message: 'Successfully deleted Task!' });
+  });
+})
+router.put('/todos/:id', function(req, res,){
+  Task.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }, (err, doc) => {
+    if (err) {
+        res.send(err);
+    }
+    res.json(doc);
+  });
+})
 
 module.exports = router;
